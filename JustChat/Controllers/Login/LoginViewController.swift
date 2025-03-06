205a91fd301bbdd1524be9685dd04cc9e21c5d5b
Alexander6Shevtsov <axrsvr@gmail.com>
2025-03-06 12:29:58 +0300

    Add SliderSlides

--- JustChat/Controllers/Login/LoginViewController.swift
+++ JustChat/Controllers/Login/LoginViewController.swift
@@ -9,20 +9,14 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
+    let slidesSlider = SliderSlides()
     var collectionView: UICollectionView!
     var slides: [Slides] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configCollectionView()
-        
-        let slide1 = Slides(id: 1, text: "text1", img: #imageLiteral(resourceName: "1"))
-        let slide2 = Slides(id: 2, text: "text1", img: #imageLiteral(resourceName: "2"))
-        let slide3 = Slides(id: 3, text: "text1", img: #imageLiteral(resourceName: "3"))
-        
-        slides.append(slide1)
-        slides.append(slide2)
-        slides.append(slide3)
+        slides = slidesSlider.getSlides()
     }
     
     func configCollectionView() {
--- JustChat/Model/SliderSlides.swift
+++ JustChat/Model/SliderSlides.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+final class SliderSlides {
+    
+    func getSlides() -> [Slides] {
+        var slides: [Slides] = []
+        
+        let slide1 = Slides(id: 1, text: "text1", img: #imageLiteral(resourceName: "1"))
+        let slide2 = Slides(id: 2, text: "text1", img: #imageLiteral(resourceName: "2"))
+        let slide3 = Slides(id: 3, text: "text1", img: #imageLiteral(resourceName: "3"))
+        
+        slides.append(slide1)
+        slides.append(slide2)
+        slides.append(slide3)
+        return slides
+    }
+}
